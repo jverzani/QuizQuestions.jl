f5b663a5fb67a5504a2345423a1ed653cd17979e
jverzani <jverzani@gmail.com>
2022-04-17 09:48:51 -0400

    add plotlylightq type for Weave

--- src/question_types.jl
+++ src/question_types.jl
@@ -443,3 +443,59 @@ function hotspotq(imgfile, xy, ΔxΔy;
                   correct_answer=nothing)
     HotspotQ(imgfile, xy, ΔxΔy, label, hint, explanation, correct_answer)
 end
+
+
+## ----
+mutable struct PlotlyLightQ <: Question
+    p
+    xs
+    ys
+    label
+    hint
+    explanation
+    correct_answer
+end
+
+"""
+    plotlylightq(p, xs=(-Inf, Inf), ys=(-Inf, Inf);
+                 label="", hint="", explanation="",
+                 correct_answer=nothing)
+
+Display a `PlotlyLight` graph. By default, correct answers select a value on the graph with `x` in the range specified by `xs` and `y` in the range specified by `ys`.
+
+* `xs`: specifies interval for selected pont `[x₀,x₁]`, defaults = `(-Inf,Inf)`
+* `ys`: range `[y₀,y₁]`
+* `correct_answer`: when speficied, allows more advanced notions of correct. This is a JavaScript code snippet with `x` and `y` representing the point on the graph that is clicked on.
+
+## Examples
+
+```
+using PlotlyLight # not loaded by default
+xs = range(0, 2pi, length=100)
+ys = sin.(xs)
+p = Plot(Config(x=xs, y=ys))
+plotlylightq(p, (3,Inf); label="Click a value with ``x>3``")
+```
+
+An example where the default grading script needs modification. Note also, the `x`, `y` values refer to the *first* graph. (One could modify their definition in `correct answer`; they are found through `x=e.points[0].x`, `y=e.points[0].y`.)
+
+```
+xs = range(0, 2pi, length=100)
+ys = sin.(xs)
+p = Plot(Config(x=xs, y=ys))
+push!(p.data, Config(x=xs, y=cos.(xs)));
+question = "Click a value where `sin(x)` is increasing"
+# evalute pi/2 as no pi in JavaScript, also Inf -> Infinity
+correct_answer = "((x >= 0 && x <= $(pi/2)) || (x >= $(3pi/2) && x <= $(2pi)))"
+plotlylightq(p, (3,Inf); label=question, correct_answer=correct_answer)
+```
+
+!!! note
+    The use of `PlotlyLight` graphics works with `Weave`, but is unusable from `Documenter`.
+
+"""
+function plotlylightq(p, xs=(-Inf, Inf), ys=(-Inf,Inf);
+                  label = "", hint="", explanation="",
+                  correct_answer=nothing)
+    PlotlyLightQ(p, xs, ys, label, hint, explanation, correct_answer)
+end
